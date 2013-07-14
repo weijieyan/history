a3cff817612beb4fdc98c8912b035f5cc3b2075a
weijieyan <361273693@gmail.com>
2013-07-14 09:06:48 +0800

    add vote

--- handlers/handler.py
+++ handlers/handler.py
@@ -7,6 +7,7 @@ Created on 2013-3-21
 '''
 import tornado.web
 from tornado.web import HTTPError
+from tornado.options import  options
 from db import MRBAC
 import random, string
 import datetime
@@ -15,6 +16,7 @@ import ujson
 import json, re, urllib
 import time, hashlib
 import sys, os
+import search_history
 class BaseHandler(tornado.web.RequestHandler):
     @property
     def db(self):
@@ -50,15 +52,51 @@ class Register(BaseHandler):
             return
         else :
             self.redirect("/register")    
-
-class Rank(BaseHandler):
+class MongthlyVote(BaseHandler):
     def get(self):
-        entries=[]
-        self.render("rank.html",entries=entries)
+        entries=self.db.query("select * from entry limit 0,20")
+        self.render("mongthlyvote.html",entries=entries)
+class Vote(BaseHandler):
+    def get(self,userid,id):
+        votes=self.db.query("select * from vote where entry_id=%s and user_id=%s",id,userid)
+        if not votes:
+            self.db.execute("insert into vote(user_id,entry_id) values(%s,%s)",userid,id)
+            self.db.execute("update entry set vote=vote+1")
+        self.redirect("/monthlyvote")
+        
+class Entry(BaseHandler):
+    def get(self,id):
+        entry=self.db.get("select * from entry where id=%s",id)
+        self.render("entry.html",entry=entry)
 class Search(BaseHandler):
     def get(self):
-        entries=[]
-        self.render("search.html",entries=entries)
+        self.render("search.html")
+    def post(self):
+        search=self.get_argument("search", None)
+        if not search:
+            self.redirect("/search")
+        querystring=search
+        matches=search_history.search(querystring)
+        self.render("search_list.html",matches=matches)
+class Ejournal(BaseHandler):
+    def get(self):
+        self.render("journal.html")
+    def post(self):
+        self.render("journal.html")
+class Download(BaseHandler):
+    @tornado.web.authenticated
+    def get(self,fileid):
+        if not fileid:
+            self.redirect("/monthlyvote")
+     
+        historyfile=self.db.get("select * from historyfile where fileid=%s",fileid)
+        
+        self.set_header('Content-Type','application/msword') 
+        self.set_header("Content-Disposition", "attachment;filename=%s"%historyfile.filename)
+         
+        with open(historyfile.file_url,'rb') as f:
+            self.write(f.read())
+        self.flush() 
 class Upload(BaseHandler):
     @tornado.web.authenticated
     def get(self):
@@ -67,11 +105,12 @@ class Upload(BaseHandler):
         title=self.get_argument("title", None)
         keywords=self.get_argument("keywords", None)
         desc=self.get_argument("desc", None)
-        filename=self.get_arguments("filename")
         area=self.get_argument("area", None)
         username=self.current_user.username
-        if title and keywords and desc and filename and area:
-            self.db.execute("INSERT INTO entry (author,entryname,keywords,description,area,add_time) VALUES (%s,%s,%s,%s,%s,Now())",username,title,keywords,desc,area)
+        fileid=self.get_argument("fileid", None)
+        if title and keywords and desc  and area and fileid:
+            self.db.execute("INSERT INTO entry (author,entryname,keywords,description,area,add_time,fileid) VALUES (%s,%s,%s,%s,%s,Now(),%s)",username,title,keywords,desc,area,fileid)
+            
             self.redirect("/")
         else:
             self.render("upload.html",login_error="不能为空")
@@ -102,16 +141,16 @@ class FileUpload(BaseHandler):
         """
         This can be ovewritten to implement your own file writing functions.
         """
-        with open(filelocation, 'w+') as f:
+        with open(filelocation, 'wb+') as f:
             f.write(data)
-
+        
     def process_file(self, file):
         filename = re.sub(r'^.*\\', '', file['filename'])
-
+        
         result = {}
-#        result['name'] = filename
-#        result['type'] = file['content_type']
-#        result['size'] = len(file['body'])
+        result['name'] = filename
+        result['type'] = file['content_type']
+        result['size'] = len(file['body'])
 
         filelocation = self.getFileLocation(filename)
 #        filetoken = self.getFileToken(filelocation)
@@ -119,6 +158,8 @@ class FileUpload(BaseHandler):
 #        result['token'] = filetoken
         result['success']=True
         self.saveFile(file['body'], filelocation)
+        result['fileid']=self.get_argument("qquuid")
+        self.db.execute("insert into historyfile(file_url,fileid,filename,filetype,filesize) values(%s,%s,%s,%s,%s)",filelocation,result['fileid'],result['name'],result['type'],result['size'])
         return result
 
     def getFileToken(self, filelocation):
@@ -140,12 +181,12 @@ class FileUpload(BaseHandler):
     def handle_upload(self):
         # XXX will this ever be bigger than 1?
         files = self.request.files['qqfile']
-        print self.get_argument("qquuid")
         for file in files:
             start_time = time.time()
 
             result = self.process_file(file)
             result['elapsed_time'] = time.time() - start_time
+           
             return result
 
     def post(self):
@@ -181,11 +222,15 @@ class AuthLogoutHandler(BaseHandler):
         self.redirect(self.get_argument("next", "/"))
 handler_urls = [
         (r"/",Home),
+         (r"/monthlyvote",MongthlyVote),
+         (r"/vote/([0-9]+)/([0-9]+)",Vote),
         (r"/search",Search),
-        (r"/rank",Rank),
         (r"/register",Register),
+        (r"/download/(.*)",Download),
         (r"/upload",Upload),
         (r"/fileupload",  FileUpload),
+        (r"/entry/([0-9]+)",  Entry),
+        
         (r"/auth/login", AuthLoginHandler),
         (r"/auth/logout", AuthLogoutHandler),
        
